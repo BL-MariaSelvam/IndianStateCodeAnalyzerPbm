918dc115df7af1a72cf375559e0c1d469cda61de
BL-MariaSelvam <maria.selvam@bridgelabz.com>
2026-01-19 17:15:10 +0530

    Added Welcome Message

---
+++ IndianStateCodeAnalyzerPbm/src/main/java/StateCodeAnalyzerMain.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class StateCodeAnalyzerMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State Code Analyzer Problem");
+
+	}
+
+}
